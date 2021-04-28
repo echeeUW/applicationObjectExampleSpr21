15490d06d0125b9419d155925b56c3603c24207c
Eric Chee <eric.chee@hulu.com>
2021-04-28 10:43:21 -0700

    Update list of emails

--- app/src/main/java/edu/uw/echee/applicationobjectexample/EmailAdapter.kt
+++ app/src/main/java/edu/uw/echee/applicationobjectexample/EmailAdapter.kt
@@ -6,20 +6,18 @@ import androidx.recyclerview.widget.RecyclerView
 import edu.uw.echee.applicationobjectexample.databinding.ItemEmailBinding
 
 class EmailAdapter(
-    private val items: List<Email>
+    private val emails: List<Email>
 ): RecyclerView.Adapter<EmailAdapter.EmailViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmailViewHolder {
         val binding = ItemEmailBinding.inflate(LayoutInflater.from(parent.context))
-
-
         return EmailViewHolder(binding)
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = emails.size
 
     override fun onBindViewHolder(holder: EmailViewHolder, position: Int) {
-        val email = items[position]
+        val email = emails[position]
 
         with(holder.binding) {
             tvEmailAddress.text = email.sender
@@ -27,7 +25,6 @@ class EmailAdapter(
         }
     }
 
-    class EmailViewHolder(val binding: ItemEmailBinding): RecyclerView.ViewHolder(binding.root) {
-    }
+    class EmailViewHolder(val binding: ItemEmailBinding): RecyclerView.ViewHolder(binding.root)
 }
 
