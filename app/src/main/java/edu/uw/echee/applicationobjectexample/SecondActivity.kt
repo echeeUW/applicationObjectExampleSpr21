5aa5fb1bd9aa8cec0de981edf5ee832f23e3f35a
Eric Chee <eric.chee@hulu.com>
2021-04-30 12:42:51 -0700

    Fixed up button to be onSupportNavigateUp instead of onNavigateUp

--- app/src/main/java/edu/uw/echee/applicationobjectexample/SecondActivity.kt
+++ app/src/main/java/edu/uw/echee/applicationobjectexample/SecondActivity.kt
@@ -2,10 +2,8 @@ package edu.uw.echee.applicationobjectexample
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.navigation.NavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import edu.uw.echee.applicationobjectexample.databinding.ActivitySecondBinding
@@ -41,5 +39,5 @@ class SecondActivity : AppCompatActivity() {
         }
     }
 
-    override fun onNavigateUp() = navController.navigateUp()
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
